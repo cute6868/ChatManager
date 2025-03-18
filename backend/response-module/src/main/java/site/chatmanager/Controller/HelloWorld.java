7091655fa915ac7bd5f59fe400402eb3107264a4
Ander <cute6868@outlook.com>
2025-03-18 21:22:58 +0800

    Fixed dependency conflicts, configured the database, and successfully ran the project.

---
+++ backend/response-module/src/main/java/site/chatmanager/Controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package site.chatmanager.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
