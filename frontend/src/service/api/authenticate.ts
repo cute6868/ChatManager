ea2326b6597ce4414480935030187336e4ad1ffa
Ander <cute6868@outlook.com>
2025-05-07 20:15:16 +0800

    The authentication business API of the front end has been completed.

---
+++ frontend/src/service/api/authenticate.ts
@@ -0,0 +1,35 @@
+import request from '../request';
+
+// ==================== 验证业务 ====================
+
+// 更新账号验证
+export function authRequestForUpdtAccount(uid: number) {
+  return request({
+    url: `/api/authenticate/${{ uid }}/update/account`,
+    method: 'post'
+  });
+}
+
+// 更新密码验证
+export function authRequestForUpdtPassword(uid: number) {
+  return request({
+    url: `/api/authenticate/${{ uid }}/update/password`,
+    method: 'post'
+  });
+}
+
+// 更新邮箱验证
+export function authRequestForUpdtEmail(uid: number) {
+  return request({
+    url: `/api/authenticate/${{ uid }}/update/email`,
+    method: 'post'
+  });
+}
+
+// 注销账号验证
+export function authRequestForDeactivateAccount(uid: number) {
+  return request({
+    url: `/api/authenticate/${{ uid }}/deactivate`,
+    method: 'post'
+  });
+}
