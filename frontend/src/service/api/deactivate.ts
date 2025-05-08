02d0c2085b9bc7eef8917a94ca208da0b6669f6a
Ander <cute6868@outlook.com>
2025-05-08 10:01:11 +0800

    The function name of the account cancellation API has been modified.

--- frontend/src/service/api/deactivate.ts
+++ frontend/src/service/api/deactivate.ts
@@ -3,7 +3,7 @@ import request from '../request';
 // ==================== 注销业务 ====================
 
 // 注销账号
-export function deactivateAccount(uid: number, verifyCode: string) {
+export function deactivateAccountRequest(uid: number, verifyCode: string) {
   return request({
     url: `/api/deactivate/${{ uid }}`,
     method: 'delete',
